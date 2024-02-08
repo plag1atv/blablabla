72470c6bc296a954332f784d5ef314b465cc5f8f
plag1atv <tlkayumov06@gmail.com>
2024-02-08 18:34:23 +0300

    огрызок

--- blablablaaaaaaaa.py
+++ blablablaaaaaaaa.py
@@ -16,6 +16,11 @@ def index2():
     with open('templates/promotion.html', 'r', encoding='utf-8') as stream:
         return stream.read()
 
+@app.route('/image_mars')
+def index3():
+    with open('templates/ogrizok.html', 'r', encoding='utf-8') as stream:
+        return stream.read()
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
