6ed9aeb57981a907f13f4104ee15e120275021aa
plag1atv <tlkayumov06@gmail.com>
2024-02-08 18:14:55 +0300

    реклама

--- blablablaaaaaaaa.py
+++ blablablaaaaaaaa.py
@@ -11,6 +11,11 @@ def index():
 def index1():
     return f'И на Марсе будут яблони цвести!'
 
+@app.route('/promotion')
+def index2():
+    with open('templates/promotion.html', 'r', encoding='utf-8') as stream:
+        return stream.read()
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
